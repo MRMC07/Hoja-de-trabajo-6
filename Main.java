17cee5f1f55136707643d74e841127d8881136a9
JuniorLancerio <juniorlancerio77@gmail.com>
2026-03-09 20:15:18 -0600

    clase Main

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        HashMapRestaurante menu = new HashMapRestaurante(10);
+
+        menu.put("Risotto de Trufa", 125.50);
+        menu.put("Salmon Teriyaki", 98.00);
+        menu.put("Pulpo a la Parrilla", 150.00);
+        menu.put("Filete Wagyu", 250.75);
+        menu.put("Pizza Margarita", 80.00);
+        menu.put("Cheesecake de Pistacho", 45.25);
+        menu.put("Langosta Mantequilla", 300.00);
+
+        menu.mostrar();
+    }
 }
